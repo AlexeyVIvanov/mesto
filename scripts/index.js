4c6ed5fbad55f783614792df3d29014317e38302
Alexey Ivanov <aivan1970@yandex.ru>
2022-01-19 21:37:19 +0600

    Added JS

---
+++ scripts/index.js
@@ -0,0 +1,55 @@
+
+let openPopupButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closePopupButton = document.querySelector('.popup__close');
+
+let formElement = document.querySelector('.popup__form');
+
+let nameInput = document.querySelector('#topfield');
+let professionInput = document.querySelector('#bottomfield');
+
+let nameProfile = document.querySelector('.profile__title');
+let professionProfile = document.querySelector('.profile__subtitle');
+
+let submitButton = document.querySelector('.popup__submit');
+
+// Открытие-закрытие Попапа
+
+openPopupButton.addEventListener('click', function () {
+  popup.classList.add('popup_opened')
+  // Проставление данных из разметки в инпуты в момент открытия
+  nameInput.value = nameProfile.textContent;
+  professionInput.value = professionProfile.textContent;
+});
+
+closePopupButton.addEventListener('click', function () {
+  popup.classList.remove('popup_opened')
+  // Сброс полей формы при закрытии
+  formElement.reset()
+});
+
+// Закрытие Попапа после нажатия кнопки Сохранить
+
+submitButton.addEventListener('click', function () {
+  popup.classList.remove('popup_opened')
+});
+
+// Обработчик «отправки» формы
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+   // Получите значение полей jobInput и nameInput из свойства value
+nameInput.textContent = nameInput.value;
+professionInput.textContent = professionInput.value;
+    // Выберите элементы, куда должны быть вставлены значения полей
+    // Вставьте новые значения с помощью textContent
+nameProfile.textContent = nameInput.value;
+professionProfile.textContent = professionInput.value;
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
